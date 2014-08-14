ba137df58e8d99faa2001408230aa759ad1b95de
Greenhouse George <amarpreet@minhas.io>
2014-08-14 14:34:38 -0400

    Added pelican, removed fabfile, fixed file locations

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Amarpreet Minhas'
+SITENAME = u'Ampx Blog'
+SITEURL = 'http://ampx.minhas.io'
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+
+PDF_GENERATOR = True
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('Github', 'http://github.com/Asara'),
+         ('Bitbucket', 'http://bitbucket.com/AmarpreetMinhas'),
+        )
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
